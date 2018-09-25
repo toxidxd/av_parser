3c92c1cedc590835fdafa242a506e730227d123a
toxidxd <toxid13@yandex.ru>
2018-09-26 01:19:03 +0300

    написаны циклы, данные парсятся. проблема с отсутствием цен.

--- avito.py
+++ avito.py
@@ -1,6 +1,5 @@
 import requests, bs4
 
-
 def page_counter (link):
     soup = requests.get(link)
     soup = bs4.BeautifulSoup(soup.text, "html.parser")
@@ -9,48 +8,90 @@ def page_counter (link):
     pages_last = int(pages_last[1])
     return pages_last
 
-def links_parser (link_p):
-    soup = requests.get(link_p)
-    soup = bs4.BeautifulSoup(soup.text, "html.parser")
-    item_links_temp = soup.select('.item-description-title-link')
-    item_links = []
-    i = 0
-    for itm in item_links_temp:
-        item_links.append("https://www.avito.ru"+item_links_temp[i].get('href'))
-        i += 1
-    return item_links
+def links_parser (link, pcount):
+    all_links = []
+    page = 1
+    while page <= pcount:
+        print ("Current page: ", page)
+        soup = requests.get(link+"?p="+str(page))
+        soup = bs4.BeautifulSoup(soup.text, "html.parser")
+        item_links_temp = soup.select('.item-description-title-link')
+        i = 0
+        for itm in item_links_temp:
+            all_links.append("https://www.avito.ru"+item_links_temp[i].get('href'))
+            i += 1
+        print ("Links parsed", len(all_links))
+        page += 1
+    return all_links
 
+def items_parser (all_links):
+    all_items = []
+
+    c_lnk = 0
+    for lnk in all_links:
+        cur_item = []
+        print("\nCur link: ", c_lnk, "\n", all_links[c_lnk])
+        soup = requests.get(all_links[c_lnk])
+        soup = bs4.BeautifulSoup(soup.text, "html.parser")
+
+        item = soup.select('.title-info-title-text')
+        item_name = item[0].getText()
+
+        price = soup.select('.price-value-string .js-item-price')
+        item_price = price[0].get('content')
+
+        descript = soup.select('.item-description-text')
+        item_descript= descript[0].getText()
+
+
+        #print(item_name)
+        #print(item_price)
+        #print(item_descript)
+        cur_item = [all_links[c_lnk], item_name, item_price, item_descript]
+        #print("\n\n\ncur!!!!!!!!!", cur_item)
+        all_items.append(cur_item)
+        #print("\n\n\nall!!!!!!!!", all_items)
+        c_lnk += 1
+
+
+
+    return all_items
 
 
 print ("Hello, pel_MEN!\nThis is avito parser.\nLink example: https://www.avito.ru/zernograd/tovary_dlya_kompyutera")
-link = input("input_link: ")
-#link = "https://www.avito.ru/zernograd/tovary_dlya_kompyutera"
+
+#link = input("input_link: ")
+link = "https://www.avito.ru/zernograd/tovary_dlya_kompyutera"
+print ("\nCounting pages")
 pcount = page_counter(link)
 print ("Count pages: ", pcount)
 
-
-all_links = []
-page = 1
-while page <= pcount:
-    print("Parsing page ", page)
-    cur_page = link+"?p="+str(page)
-    #print(links_parser(cur_link))
-    parsed_links = links_parser(cur_page)
-    #print(parsed_links)
-    x = 0
-    for itm in parsed_links:
-        all_links.append(parsed_links[x])
-        x += 1
-    print("links parsed ", len(links_parser(cur_page)))
-    page += 1
-
-zz = 0
-print("==================================")
-for itm in all_links:
-    print(all_links[zz])
-    zz += 1
-print(all_links)
+print ("\nParsing links")
+all_links = links_parser(link, pcount)
 print("Overal links parsed: ", len(all_links))
+
+print("\nPasring items\n")
+all_items = items_parser(all_links)
+print("Overal items parser: ", len(all_items))
+
+
+
+
+
+#zz = 0
+#for itm in all_links:
+#    print(all_links[zz])
+#    zz += 1
+#print(all_links)
+
+
+#zz = 0
+#print("==================================")
+#for itm in all_links:
+    #print(all_links[zz])
+    #zz += 1
+#print(all_links)
+#print("Overal links parsed: ", len(all_links))
 
 #print(links_parser(link))
 #print(len(links_parser(link)))
