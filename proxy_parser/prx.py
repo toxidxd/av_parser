9c04f91e41cf028eb5b7743a90b5b4ac9f0db633
toxidxd <toxid13@yandex.ru>
2020-12-01 18:38:37 +0300

    PEP updates

--- proxy_parser/prx.py
+++ proxy_parser/prx.py
@@ -7,40 +7,41 @@ from fake_useragent import UserAgent
 
 link = "https://hidemyna.me/ru/proxy-list/?country=RU&maxtime=2000&type=hs#list"
 
-print("Receive page...")
 scraper = cfscrape.create_scraper()
+
 page = scraper.get(link)
 
-print("Page parsing...")
-time.sleep(0.5)
 soup = bs4.BeautifulSoup(page.text, "html.parser")
+#print(soup)
+#prx = soup.select('.proxy__t')
+
+#prx = soup.select(".d")
+#print(soup)
+
 soup = str(soup)
 
-print("Searching proxies...")
-time.sleep(0.5)
 reg_res = re.findall("\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}</td><td>\d{1,5}", soup)
+print(len(reg_res))
+print(type(reg_res))
+print(reg_res)
 
-print("Splitting...")
-time.sleep(0.5)
+print("----splitting----")
 all_prx = []
+
 for prx in reg_res:
     cur_prx = re.split("</td><td>", prx)
     #print(res2)
     all_prx.append(cur_prx)
 
-print("Proxies parsed: ", len(all_prx))
-time.sleep(0.5)
 
-print("Writting to csv file...")
-time.sleep(0.5)
 with open("proxies.csv", "w", newline='') as csv_file:
     writer = csv.writer(csv_file, delimiter=',')
-    writer.writerow(["Protocol", "IP:port"])
+    writer.writerow(["IP", "port"])
     for line in all_prx:
+        print(line)
+        print(type(line))
         writer.writerow(line)
 
-print("Write complete!\nEND")
-time.sleep(0.5)
 
 #i=0
 #for x in prx:
